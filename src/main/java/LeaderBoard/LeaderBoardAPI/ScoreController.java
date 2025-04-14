26044ca60727c75b7823a4f4ab922d03ce320e72
Mason Beale <mbeale@cedarville.edu>
2025-04-14 19:30:39 -0400

    implement GetTopTenScores and GetAllScores

--- src/main/java/LeaderBoard/LeaderBoardAPI/ScoreController.java
+++ src/main/java/LeaderBoard/LeaderBoardAPI/ScoreController.java
@@ -1,6 +1,8 @@
 package LeaderBoard.LeaderBoardAPI;
 
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -9,6 +11,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import LeaderBoard.LeaderBoardAPI.Models.Score;
+import LeaderBoard.LeaderBoardAPI.Models.User;
 import LeaderBoard.LeaderBoardAPI.Repos.ScoreRepository;
 
 @RestController
@@ -23,4 +26,28 @@ public class ScoreController {
         return userScores;
     }
 
+    @GetMapping("/topten")
+    public ArrayList<Score> GetTopTenUserScores() {
+        ArrayList<Score> userScores = (ArrayList) scoreRepository.findAll();
+        
+        Collections.sort(userScores, new Comparator<Score>() {
+            public int compare(Score scoreOne, Score scoreTwo) {
+                return scoreTwo.getScore() - scoreOne.getScore();
+            }
+        });        
+
+        ArrayList<Score> topTenScores = new ArrayList<>();
+        for (int index = 0; index < 10; index++){
+            topTenScores.add(userScores.get(index));
+        }
+
+        return topTenScores;
+    }
+
+    @GetMapping()
+    public ArrayList<Score> GetAllUserScores() {
+        ArrayList<Score> userScores = (ArrayList) scoreRepository.findAll();
+
+        return userScores;
+    }
 }
